bd2ebeb730b0de9af4722fd333db635c830cc462
shingo.hatashi <hatashi0972015@icloud.com>
2020-09-17 22:07:17 +0900

    Initial Commit

---
+++ kari/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kari
+//
+//  Created by 幡司真吾 on 2020/09/17.
+//  Copyright © 2020 shingo.hatashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
